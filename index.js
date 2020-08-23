6b20e2adaa85f8327b130d45b6b50a1bb243dac3
Anthony Moreno <tonymoreno86@gmail.com>
2020-08-22 21:09:54 -0700

    resetting index.js file

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-console.log(process.env);
-
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
